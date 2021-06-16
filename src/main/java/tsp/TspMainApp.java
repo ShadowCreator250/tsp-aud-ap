1ac0e4c62ca79c4d1dfa860cf985c9b0b848e678
phillipeckstein <phillipeckstein@pop-os.localdomain>
2021-06-16 17:17:51 +0200

    Add simple MainView

--- src/main/java/tsp/TspMainApp.java
+++ src/main/java/tsp/TspMainApp.java
@@ -1,6 +1,11 @@
 package tsp;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class TspMainApp extends Application {
@@ -15,6 +20,20 @@ public class TspMainApp extends Application {
 	public void start(Stage primaryStage) {
     this.primaryStage = primaryStage;
     primaryStage.setTitle("TSP App");
+
+    FXMLLoader loader = new FXMLLoader();
+    loader.setLocation(TspMainApp.class.getResource("view/MainView.fxml"));
+
+    BorderPane rootElement;
+    try {
+      rootElement = loader.load();
+      Scene scene = new Scene(rootElement);
+      primaryStage.setScene(scene);
+      primaryStage.show();
+    } catch(IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
 
     primaryStage.show();
 	}
