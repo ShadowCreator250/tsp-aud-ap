956f4ea6971bec3abdd2af62dd956771221c5dbc
phillipeckstein <phillipeckstein@pop-os.localdomain>
2021-06-17 23:15:32 +0200

    Implement Suggested Changes

--- src/main/java/tsp/model/Point.java
+++ src/main/java/tsp/model/Point.java
@@ -3,12 +3,19 @@ package tsp.model;
 public class Point {
 
   private int x;
-
   private int y;
 
   public Point(int x, int y) {
     this.x = x;
     this.y = y;
+  }
+
+  public int getX() {
+    return x;
+  }
+
+  public int getY() {
+    return y;
   }
 
 }
