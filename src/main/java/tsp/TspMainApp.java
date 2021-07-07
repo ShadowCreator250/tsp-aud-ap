5e0fb1cb72d05e6eef7bdc76037b5ef02d20c920
ShadowCreator250 <shadowcreator250+github@gmail.com>
2021-07-07 18:28:47 +0200

    code cleanup & add try-catch around file parser
    now displays error pop-up if error occurs

--- src/main/java/tsp/TspMainApp.java
+++ src/main/java/tsp/TspMainApp.java
@@ -5,8 +5,12 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.stage.Window;
+
 import tsp.view.MainViewController;
 
 public class TspMainApp extends Application {
@@ -17,8 +21,8 @@ public class TspMainApp extends Application {
     // TODO Auto-generated constructor stub
   }
 
-	@Override
-	public void start(Stage primaryStage) {
+  @Override
+  public void start(Stage primaryStage) {
     this.primaryStage = primaryStage;
     primaryStage.setTitle("TSP App");
 
@@ -41,11 +45,20 @@ public class TspMainApp extends Application {
     }
 
     primaryStage.show();
-	}
+  }
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+  public void showAlert(AlertType alertType, Window owner, String title, String header, String context) {
+    Alert alert = new Alert(alertType);
+    alert.initOwner(owner);
+    alert.setTitle(title);
+    alert.setHeaderText(header);
+    alert.setContentText(context);
+    alert.showAndWait();
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
 
   public Stage getPrimaryStage() {
     return primaryStage;
--- src/main/java/tsp/view/MainViewController.java
+++ src/main/java/tsp/view/MainViewController.java
@@ -1,13 +1,18 @@
 package tsp.view;
 
 import java.io.File;
+import java.io.IOException;
 
 import javafx.fxml.FXML;
 import javafx.scene.chart.ScatterChart;
 import javafx.scene.chart.XYChart.Data;
 import javafx.scene.chart.XYChart.Series;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.FileChooser;
+
 import tsp.TspMainApp;
+import tsp.model.Point;
+import tsp.util.CSVFormatException;
 import tsp.util.CSVUtil;
 
 public class MainViewController {
@@ -22,27 +27,31 @@ public class MainViewController {
   @FXML
   private void initialize() {
     chart.getData()
-         .add(new Series<Number, Number>());
+         .add(new Series<>());
   }
-  
+
+  public void setMain(TspMainApp tspMainApp) {
+    this.main = tspMainApp;
+  }
+
   @FXML
   private void handleLoadButton() {
     FileChooser fileChooser = new FileChooser();
     fileChooser.setTitle("Open Resource File");
     File file = fileChooser.showOpenDialog(main.getPrimaryStage());
-    CSVUtil.readPointsFromFile(file.getAbsolutePath())
-           .forEach(c -> {
-             chart.getData()
-                  .get(0)
-                  .getData()
-                 .add(new Data<Number, Number>(c.getX(), c.getY()));
-           });
-
-  }
-
-  public void setMain(TspMainApp tspMainApp) {
-    this.main = tspMainApp;
-
+    try {
+      CSVUtil.readPointsFromFile(file.getAbsolutePath())
+             .forEach((Point c) -> chart.getData()
+                                        .get(0)
+                                        .getData()
+                                        .add(new Data<>(c.getX(), c.getY())));
+    } catch(IOException e) {
+      main.showAlert(AlertType.ERROR, main.getPrimaryStage(), "Error reading file", "An Error occured while reading a file.",
+          e.getClass().getSimpleName() + ": " + e.getMessage());
+    } catch(CSVFormatException e) {
+      main.showAlert(AlertType.ERROR, main.getPrimaryStage(), "CSV Format Error", "There is an error with the CSV format.",
+          e.getClass().getSimpleName() + ": " + e.getMessage());
+    }
   }
 
 }
