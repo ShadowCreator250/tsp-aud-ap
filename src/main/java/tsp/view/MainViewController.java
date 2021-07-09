26e48343a3ca42b0a652a8a033a5ae0055368bba
ShadowCreator250 <shadowcreator250+github@gmail.com>
2021-07-09 21:21:12 +0200

    add quickload of csv, clear chart before adding points
    - Add ComboBox to choose what to load: a file from the file chooser or one of the csv files under src/main/resources/.
    - If FileChooser was terminated (closed/canceled) without choosing a file, it would throw a NPE. This commit fixes that.
    - the ScatterChart was never cleared so loading a few resources after each other added more and more points. The chart is now cleared everytime before adding points.
    - add set of 12 points (csv file) from video

--- src/main/java/tsp/view/MainViewController.java
+++ src/main/java/tsp/view/MainViewController.java
@@ -1,13 +1,17 @@
 package tsp.view;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
 
 import javafx.fxml.FXML;
 import javafx.scene.chart.ScatterChart;
 import javafx.scene.chart.XYChart.Data;
 import javafx.scene.chart.XYChart.Series;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ChoiceBox;
 import javafx.stage.FileChooser;
 
 import tsp.TspMainApp;
@@ -17,15 +21,21 @@ import tsp.util.CSVUtil;
 
 public class MainViewController {
 
+  private static final String FILE_CHOOSER_LABEL = "FileChooser";
+
   private TspMainApp main;
 
   @FXML
   private ScatterChart<Number, Number> chart;
-
-  //private Series<Number, Number> data;
+  @FXML
+  private ChoiceBox<String> loadActionSelect;
 
   @FXML
   private void initialize() {
+    loadActionSelect.getItems().add(FILE_CHOOSER_LABEL);
+    loadActionSelect.getItems().addAll(getCSVFilesUnderBinMain());
+    loadActionSelect.getSelectionModel().select(0);
+
     chart.getData()
          .add(new Series<>());
   }
@@ -36,22 +46,56 @@ public class MainViewController {
 
   @FXML
   private void handleLoadButton() {
+    String selectedItem = loadActionSelect.getSelectionModel().getSelectedItem();
+    if(selectedItem != null) {
+      if(selectedItem.equals(FILE_CHOOSER_LABEL)) {
+        showFileChooser();
+      } else {
+        File file = new File(MainViewController.class.getClassLoader().getResource(selectedItem).getPath()).getAbsoluteFile();
+        readPointsFromFileWithErrorHandling(file);
+      }
+    }
+  }
+
+  public String[] getCSVFilesUnderBinMain() {
+    File absolutePathToMainClassInBin = new File(
+        TspMainApp.class.getResource(TspMainApp.class.getSimpleName() + ".class").getPath()).getAbsoluteFile();
+    String absolutePathToPackageOfMain = absolutePathToMainClassInBin.getParent();
+    File binMainDir = Path.of(absolutePathToPackageOfMain, "./../").normalize().toFile();
+    if(binMainDir.isDirectory()) {
+      FilenameFilter csvFileFilter = (dir, name) -> name.toLowerCase().endsWith(".csv");
+      return binMainDir.list(csvFileFilter);
+    }
+    return new String[0];
+  }
+
+  private void showFileChooser() {
     FileChooser fileChooser = new FileChooser();
     fileChooser.setTitle("Open Resource File");
+    fileChooser.setInitialDirectory(new File("").getAbsoluteFile());
     File file = fileChooser.showOpenDialog(main.getPrimaryStage());
-    try {
-      CSVUtil.readPointsFromFile(file.getAbsolutePath())
-             .forEach((Point c) -> chart.getData()
-                                        .get(0)
-                                        .getData()
-                                        .add(new Data<>(c.getX(), c.getY())));
-    } catch(IOException e) {
-      main.showAlert(AlertType.ERROR, main.getPrimaryStage(), "Error reading file", "An Error occured while reading a file.",
-          e.getClass().getSimpleName() + ": " + e.getMessage());
-    } catch(CSVFormatException e) {
-      main.showAlert(AlertType.ERROR, main.getPrimaryStage(), "CSV Format Error", "There is an error with the CSV format.",
-          e.getClass().getSimpleName() + ": " + e.getMessage());
+    readPointsFromFileWithErrorHandling(file);
+  }
+
+  private void readPointsFromFileWithErrorHandling(File file) {
+    if(file != null) {
+      try {
+        addPointsToChart(CSVUtil.readPointsFromFile(file.getAbsolutePath()));
+      } catch(IOException e) {
+        main.showAlert(AlertType.ERROR, main.getPrimaryStage(), "Error reading file", "An Error occured while reading a file.",
+            e.getClass().getSimpleName() + ": " + e.getMessage());
+      } catch(CSVFormatException e) {
+        main.showAlert(AlertType.ERROR, main.getPrimaryStage(), "CSV Format Error", "There is an error with the CSV format.",
+            e.getClass().getSimpleName() + ": " + e.getMessage());
+      }
     }
+  }
+
+  private void addPointsToChart(List<Point> points) {
+    chart.getData().get(0).getData().clear();
+    points.forEach((Point c) -> chart.getData()
+                                     .get(0).getData()
+                                     .add(new Data<>(c.getX(), c.getY())));
   }
 
 }
