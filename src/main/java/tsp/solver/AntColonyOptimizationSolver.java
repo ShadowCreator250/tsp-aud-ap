0309e3bfdd97518ae79e4ded0f45bb386822b595
ShadowCreator250 <shadowcreator250+github@gmail.com>
2021-07-17 00:35:18 +0200

    add simulation constants

--- src/main/java/tsp/solver/AntColonyOptimizationSolver.java
+++ src/main/java/tsp/solver/AntColonyOptimizationSolver.java
@@ -22,8 +22,42 @@ public class AntColonyOptimizationSolver extends TspSolver {
     TspSolver solver = new AntColonyOptimizationSolver(points);
     solver.solve();
     solver.printSolution();
-
   }
+
+  /**
+   * Controls the extent to which ants will prefer nearby points.
+   * Too high and algorithm will essentially be a greedy search,
+   * but too low and the search will likely stagnate.
+   */
+  private static final double DISTANCE_POWER = 4.0;
+
+  /**
+   * Controls how likely each ant will be to follow the same paths
+   * as the ants be fore it. Too high and algorithm will keep searching
+   * the same path, too low and it will search too many paths.
+   */
+  private static final double PHEROMONE_POWER = 1.0;
+
+  /**
+   * Intensity of Pheromone trail
+   */
+  private static final double PHEREMONE_INTENSITY = 10;
+
+  /**
+   * Initial Pheromone strength along all paths
+   * (otherwise initial probabilities will all be zero)
+   */
+  private static final double INITIAL_PHEREMONE_INTENSITY = 1;
+
+  /**
+   * Proportion of Pheromone to evaporate each step
+   */
+  private static final double EVAPORATION_RATE = 0.3;
+
+  /**
+   * Number of ants in each group
+   */
+  private static final int ANT_GROUP_SIZE = 20;
 
   public AntColonyOptimizationSolver(List<Point> points) {
     super(points);
